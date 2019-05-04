09bc4ec95af01ba8961f16a5be362f3766b46b3c
lgfahys <lindsey@codeasite.com>
2019-05-04 17:21:37 -0400

    completed html routes

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("app/public"));
+
+// require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
