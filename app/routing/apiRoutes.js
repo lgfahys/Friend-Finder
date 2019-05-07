ccba25fd2908410c75ca17271803daea78d72cf5
lgfahys <lindsey@codeasite.com>
2019-05-07 14:27:08 -0400

    added user validation and modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,7 @@
 //    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 //    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will 
 //    * also be used to handle the compatibility logic.
-var users = require("../data/friends");
+var users = require("../data/friends.js");
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
@@ -10,8 +10,20 @@ module.exports = function(app) {
       });
 
       app.post("/api/friends", function(req, res) {
-          users.push(req.body);
+          var userInput = req.body;
+          var userScores = req.body.scores;
+          var difference = 0;
+          var match = {
+              name: "",
+              photo: "",
+              compatability: 1000
+          }
+
+          var x = userScores.map(function(item) {
+              return parseInt(item, 10);
+          });
+
+          users.push(userInput);
           res.json(true);
-   
       });
 };
