65d28b2c3a98523e6ac6ef77629afdbae548b157
lgfahys <lindsey@codeasite.com>
2019-05-08 23:45:56 -0400

    added styling

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,103 @@
 var users = [
     {
-        name: "Elmo",
-        photo: "https://upload.wikimedia.org/wikipedia/en/7/74/Elmo_from_Sesame_Street.gif",
+        name: "Rachel Green",
+        photo: "https://vignette.wikia.nocookie.net/friends/images/7/75/Rachel_gray_t-shirt.jpg/revision/latest/scale-to-width-down/200?cb=20190210054726",
         "scores": [
-            5,
             1,
             4,
-            4,
+            3,
+            3,
             5,
+            4,
             1,
             2,
+            3,
+            3
+        ]
+    },
+
+    {
+        name: "Pheobe Buffay",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Friendsphoebe.jpg/220px-Friendsphoebe.jpg",
+        "scores": [
+            4,
+            2,
+            3,
+            4,
+            2,
+            2,
+            1,
             5,
+            2,
+            1
+        ]
+    },
+
+    {
+        name: "Ross Geller",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/David_Schwimmer_as_Ross_Geller.jpg/220px-David_Schwimmer_as_Ross_Geller.jpg",
+        "scores": [
+            3,
+            4,
+            3,
+            2,
+            2,
+            1,
+            1,
+            3,
+            3,
+            3
+        ]
+    },
+
+    {
+        name: "Chandler Bing",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/6/6c/Matthew_Perry_as_Chandler_Bing.jpg/220px-Matthew_Perry_as_Chandler_Bing.jpg",
+        "scores": [
+            2,
+            3,
+            4,
+            5,
+            5,
+            2,
+            3,
+            1,
+            2,
+            3
+        ]
+    },
+
+    {
+        name: "Monica Geller",
+        photo: "https://i.pinimg.com/originals/05/b9/6d/05b96d032015c33869ac88c9767f1683.jpg",
+        "scores": [
+            3,
+            3,
+            2,
+            1,
+            1,
+            1,
+            2,
+            3,
             4,
             1
         ]
     },
 
     {
-        name: "Big Bird",
-        photo: "https://www.wdse.org/sites/default/files/imagecache/standard/images/pages/bigbird.jpg",
+        name: "Ben Geller",
+        photo: "https://vignette.wikia.nocookie.net/friends/images/8/8e/Ben.png/revision/latest?cb=20180324151610",
         "scores": [
-            3,
-            1,
-            3,
-            4,
-            5,
-            1,
-            1,
-            5,
             2,
-            1
-        ]
-    },
-
-    {
-        name: "Cookie Monster",
-        photo: "https://ewedit.files.wordpress.com/2017/08/cm-p022-sst.jpg",
-        "scores": [
-            4,
             3,
             2,
-            1,
             5,
             4,
             3,
-            3,
             2,
-            1
+            2,
+            3,
+            3
         ]
     }
 ]
